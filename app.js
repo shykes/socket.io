7e5fcf9f6744c3474669ca274cdeeb1b35218cce
ctran <cuong.tran@gmail.com>
2011-06-30 00:16:25 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express',
+    layout: false
+  });
+});
+
+/**
+ * Socket.IO server (single process only)
+ */
+var io = require('socket.io').listen(app), nicknames = {};
+
+io.set('transports', [
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('user message', function (msg) {
+    socket.broadcast.emit('user message', socket.nickname, msg);
+  });
+
+  socket.on('nickname', function (nick, fn) {
+    if (nicknames[nick]) {
+      fn(true);
+    } else {
+      fn(false);
+      nicknames[nick] = socket.nickname = nick;
+      socket.broadcast.emit('announcement', nick + ' connected');
+      io.sockets.emit('nicknames', nicknames);
+    }
+  });
+
+  socket.on('disconnect', function () {
+    if (!socket.nickname) return;
+
+    delete nicknames[socket.nickname];
+    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+    socket.broadcast.emit('nicknames', nicknames);
+  });
+});
+
+// Only listen on $ node app.js
+if (!module.parent) {
+  app.listen(8080);
+  console.log("Express server listening on port %d", app.address().port);
+}
